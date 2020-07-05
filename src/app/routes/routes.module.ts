7135885accc7a71b0fc24c188cd73dfea6b257b5
yosephNoh <yoseph.noh@solarconnect.kr>
2020-07-05 18:01:17 +0900

    주변 개발중

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,33 +1,51 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-import { LayoutComponent } from '@app/layout/layout.component';
-import { HeaderComponent } from '@app/layout/component/header/header.component';
-import { CitiesComponent } from './cities/cities.component';
-import { HomeComponent } from './home/home.component';
-import { FestivalComponent } from './festival/festival.component';
-import { CourseComponent } from './course/course.component';
-import { StayComponent } from './stay/stay.component';
-import { SearchComponent } from './search/search.component';
-import { RestaurantComponent } from './restaurant/restaurant.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Routes, RouterModule } from "@angular/router";
+import { LayoutComponent } from "@app/layout/layout.component";
+import { HeaderComponent } from "@app/layout/component/header/header.component";
+import { CitiesComponent } from "./cities/cities.component";
+import { HomeComponent } from "./home/home.component";
+import { FestivalComponent } from "./festival/festival.component";
+import { CourseComponent } from "./course/course.component";
+import { StayComponent } from "./stay/stay.component";
+import { SearchComponent } from "./search/search.component";
+import { RestaurantComponent } from "./restaurant/restaurant.component";
+import { AroundComponent } from './around/around.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: LayoutComponent,
     children: [
-      {path: '', component: HomeComponent, pathMatch: 'full', data: { animation: 'pageHome'} },
-      {path: 'search', component: SearchComponent, data: { animation: 'pageSearch'} },
-    ]
-  }
+      {
+        path: "",
+        component: HomeComponent,
+        pathMatch: "full",
+        data: { animation: "pageHome" },
+      },
+      {
+        path: "search",
+        component: SearchComponent,
+        data: { animation: "pageSearch" },
+      },
+      { path: "around", component: AroundComponent },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forRoot(routes)
-  ],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  declarations: [HeaderComponent, CitiesComponent, HomeComponent, FestivalComponent, CourseComponent, StayComponent, SearchComponent, RestaurantComponent]
+  declarations: [
+    HeaderComponent,
+    CitiesComponent,
+    HomeComponent,
+    FestivalComponent,
+    CourseComponent,
+    StayComponent,
+    SearchComponent,
+    RestaurantComponent,
+    AroundComponent,
+  ],
 })
-export class RoutesModule { }
+export class RoutesModule {}
---
+++ src/app/share/service/around.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { HttpService } from "./http.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AroundService {
+  serviceLabel = "locationBasedList";
+  constructor(private httpService: HttpService) {}
+
+  /**
+   * 위치를 기반으로 주변 관광정보를 받아옵니다.
+   * @param mapX
+   * @param mapY
+   * @param range
+   */
+  locationBasedList(mapY, mapX, range = 2000) {
+    return new Promise((resolve, reject) => {
+      const param = {
+        mapX,
+        mapY,
+        range,
+      };
+      this.httpService.get(this.serviceLabel, param).subscribe(
+        (result) => {
+          resolve(result);
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+}
